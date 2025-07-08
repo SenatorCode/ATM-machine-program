02657313b56294a3f526b5c64c47e47378153a17
Senator's Code <folamideibikunle@gmail.com>
2025-07-08 16:08:07 +0100

    Created the welcome message part of the program
    successfully imports the functions.py file into the main file

---
+++ body.py
@@ -0,0 +1,21 @@
+import functions
+sessionActive = True 
+
+while sessionActive == True :
+    print ("Welcome to Alex & Co Bank.")
+    print("""
+    Press 1 to log into your bank account
+    Press 2 to create a new bank account
+    Press 3 to exit 
+""")
+
+    userInput = input("Please enter your choice: ")
+    if userInput == 1:
+        functions.accountLogIn()
+    elif userInput ==2:
+        functions.newAccount()
+    elif userInput == 3:
+        print("Thank you for banking with us, See you next time.")
+        sessionActive = False
+    else: 
+        print("Invalid selection, please try again")
