1832cfac40853f6ec9d2dad917dd2dac36f82a4e
Senator's Code <folamideibikunle@gmail.com>
2025-07-09 12:51:39 +0100

    The function to log in has been created the security is tight as it allows only 3 attempts it can detect unregistered accounts

--- functions.py
+++ functions.py
@@ -13,7 +13,7 @@ def createNewAccount():
         return
     
     userAccount= ""
-    accType = int(input("Enter the type of account you want (1=savings, 2=current, 3=student): "))
+    accType = int(input("Enter the type of account you want (1= savings, 2= current, 3= student): "))
     if accType == 1:
         userAccount= "Savings"
     elif accType == 2:
@@ -33,17 +33,55 @@ def createNewAccount():
         "Locked": False,
         "Account Number": str(accountNumber)
     }
-
     try:
         with open("data.json", "r") as dataFile:
             accounts= json.load(dataFile)
-    except emptyFileError:
+    except FileNotFoundError:
         accounts= []
-    
     accounts.append(newAccount)
 
     with open("data.json", "w") as dataFile:
-        json.dump(accounts, dataFile)
+        json.dump(accounts, dataFile, indent=4)
     print("Your account has been created successfully. Thank You for choosing us. Happy Banking!!!")
 
+
+
+
 def accountLogIn():
+    userAccount = input("Enter your account number: ")
+    try:
+        with open("data.json", "r") as datafile:
+            accounts = json.load(datafile)
+    except FileNotFoundError:
+        accounts= []
+    
+    for account in accounts:
+        if userAccount == account["Account Number"]:
+            if account["Locked"] != False:
+                print("Your account has been blocked, please reach out to our nearest branch to reactivate your account.")
+                return
+            attempt = 0
+            print("Welcome " + account["Fullname"])
+            while attempt < 3:
+                userPin = getpass.getpass("Enter your pin: ")
+                if userPin == account["pin"]:
+                    print("You have log into your account successfully")
+                    return
+                else:
+                    attempt +=1
+                    attemptLeft = 3 - attempt
+                    print(f"You have {attemptLeft} more trials!!!")
+            print("You have entered incorrect pins 3 times, your account has been locked, reach out to our nearest bank to reactivate your account.")
+            account["Locked"] = True
+            with open("data.json", "w") as dataFile:
+                json.dump(accounts, dataFile, indent=4)
+            return
+
+        else:
+            print("The account number you enter does not exist")
+            userInput = input("Do you want to create a new account, Y/N")
+            if userInput.upper()== "Y":
+                createNewAccount()
+            else:
+                print("Have a nice day!!!")
+                return
